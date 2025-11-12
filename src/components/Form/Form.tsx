be28fe5752d9842657370eb4177b4417c57b9df8
José Pach <Paching12@icloud.com>
2025-11-11 20:30:28 -0600

    remove useless comment from Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -30,7 +30,6 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
       </div>
 
       <div className={styles.row}>
