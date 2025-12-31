e110f1a0610ccd756f972bd87ad67cf9c950b4d5
José Pach <Paching12@icloud.com>
2025-12-30 22:23:42 -0600

    Flag-icons third party lib + dinamyc import only on /app

--- src/pages/AppLayout/AppLayout.tsx
+++ src/pages/AppLayout/AppLayout.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import { User } from "@components/User";
 import { Maps } from "../../components/Maps";
 import { Sidebar } from "../../components/Sidebar";
 import styles from "./AppLayout.module.css";
 
 const AppLayout = () => {
+  useEffect(() => {
+    import("flag-icons/css/flag-icons.min.css");
+  }, []);
+
   return (
     <div className={styles.app}>
       <Sidebar />
